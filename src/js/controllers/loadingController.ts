4b7f6216d0ae784b85d767f23ed851e1354d3440
Aman Bhimani <amanb014@gmail.com>
2017-09-26 18:06:04 -0400

    Added comments to loadingController.ts

--- src/js/controllers/loadingController.ts
+++ src/js/controllers/loadingController.ts
@@ -1,3 +1,10 @@
+/****************************************************************
+File: loadingController.ts
+Purpose: Controller that manages loading the Angular views. If the computer
+is really slow, then this page will just say "Loading" until the other data loads.
+Author: Aman Bhimani
+*****************************************************************/
+
 import {ipcRenderer} from 'electron';
 
 require('angular').module('viewTube')
@@ -7,6 +14,8 @@ function loadingController($scope, $location) {
 
 	console.log('loading...');
 
+	//once the config is loaded, we change the "location" of the window to /home
+	//to now load the home page.
 	ipcRenderer.on('config-loaded', (event, data) => {
 		console.log('received event to load home page');
 		$location.url('/home');
