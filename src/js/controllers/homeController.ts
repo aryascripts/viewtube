61d22f06d7eeb1c7cf0b45501c6e755fac852b3f
Aman Bhimani <amanb014@gmail.com>
2017-09-26 18:04:41 -0400

    Added comments to homeController.ts

--- src/js/controllers/homeController.ts
+++ src/js/controllers/homeController.ts
@@ -1,16 +1,24 @@
-import {shell} from 'electron';
+/****************************************************************
+File: homeController.js
+Purpose: The controller for the first page which is the "home". 
+The home is the one that displays all the playlists that are in the system.
+Author: Aman Bhimani
+*****************************************************************/
 
+import {shell} from 'electron';
 require('angular').module('viewTube')
 .controller('homeController', homeController);
 
 function homeController($scope, shared, $window) {
+	
+	//Set scope variables for AngularJS
 	$scope.plists = shared.getPlaylists();
-
 	$scope.displayMsg = true;
 	$scope.showDesc = shared.config().showDesc;
 
 	console.log(shared.getPlaylists());
 
+	//Updates the playlists from the main service and updates the Angular view
 	var updatePlaylists = () => {
 		$scope.plists = shared.getPlaylists();
 		if($scope.plists !== undefined && $scope.plists.length > 0) {
@@ -26,6 +34,7 @@ function homeController($scope, shared, $window) {
 
 	shared.registerObserver(updatePlaylists);
 
+	//Removes the playlist from the home page and the object/storage as well
 	$scope.remove = function(n) {
 		if(confirm('Remove playlist ' + shared.getPlaylists()[n].title + '?')) {
 			let temp = shared.getPlaylists();
@@ -34,6 +43,7 @@ function homeController($scope, shared, $window) {
 		};
 	}
 
+	//Opens the Video in a new window (sends to the main node process)
 	$scope.goToYouTube = (id) => {
 		shell.openExternal('https://www.youtube.com/playlist?list='+id);
 	}
