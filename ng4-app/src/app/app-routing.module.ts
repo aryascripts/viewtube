177d46447a54bbfba12935fb18acd002d99b552a
Aman Bhimani <amanb014@gmail.com>
2017-10-10 18:07:47 -0400

    settings page kinda implemented - no functionality so far, just the template

--- ng4-app/src/app/app-routing.module.ts
+++ ng4-app/src/app/app-routing.module.ts
@@ -1,17 +1,13 @@
 import { HomeComponent } from './components/home/home.component';
 import { LoadingComponent } from './components/loading/loading.component';
+import { SettingsComponent } from './components/settings/settings.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: LoadingComponent
-},
-    {
-    path: 'home',
-    component: HomeComponent
-}
+    {path: '', component: LoadingComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'settings', component: SettingsComponent}
 ];
 
 @NgModule({
---
+++ ng4-app/src/app/components/settings/settings.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { SharedService } from './../../providers/shared.service/shared.service';
+
+@Component({
+    templateUrl: './settings.component.html',
+    selector: 'app-settings',
+    styleUrls: ['./../../../assets/css/style.css']
+})
+export class SettingsComponent {
+
+    activeTab:number;
+    
+    constructor(private shared:SharedService) {
+        this.activeTab = 0;
+    }
+
+    tabChange(n) {
+        this.activeTab = n;
+    }
+
+    
+    
+}
