900dfb55e6a7f083661d0ad98d236342b33eb5ab
Aman Bhimani <amanb014@gmail.com>
2018-03-03 23:14:07 -0500

    oauth2 process improvements

--- ng4-rewrite/googleapi/authService.ts
+++ ng4-rewrite/googleapi/authService.ts
@@ -6,17 +6,21 @@ const querystring = require('querystring');
 import { authKeys } from './auth';
 
 export default class AuthService {
+	
 	static token:any;
+	
 	static SCOPES:string[] = [
 		'https://www.googleapis.com/auth/youtube',
 		'https://www.googleapis.com/auth/youtube.readonly',
 		'https://www.googleapis.com/auth/youtube.upload'
 	];
+	
 	static oAuth2Client:any = new OAuth2Client(
 		authKeys.client_id,
 		authKeys.client_secret,
 		authKeys.redirect_uris[0]
 	);
+	
 	static authWin:BrowserWindow = null;
 	
 	static getAuthUrl(client) {
@@ -27,6 +31,7 @@ export default class AuthService {
 		});
 	}
 
+	//authorization popup and events/url navigation
 	static createAuthWindow() {
 		if(this.authWin) return; //window already exists.
 		this.authWin = new BrowserWindow({
@@ -35,18 +40,16 @@ export default class AuthService {
       'minWidth': 400,
       'minHeight': 600,
       'acceptFirstMouse': true,
-      alwaysOnTop: true
+      alwaysOnTop: true //cannot escape
 		});
+
 		this.authWin.loadURL(this.getAuthUrl(this.oAuth2Client));
 		this.authWin.on('closed', this.onClose);
 
+		//detects when the user has completed authorization
 		this.authWin.webContents.on('will-navigate', (event, url) => {
-			this.handleNavigate(url);
+			this.handleNavigate(url); //url goes to 
 		});
-
-		// this.authWin.webContents.on('did-get-redirect-request', (event, oldUrl, newUrl) => {
-		// 	this.handleNavigate(newUrl);
-		// })
 	}
 
 	static onClose() {
@@ -56,13 +59,15 @@ export default class AuthService {
 
 	static async handleNavigate(newUrl:string) {
 		const parsed = url.parse(newUrl, true);
-
+		
 		if(parsed) {
-			if(parsed.error) {
+			if(parsed.error) { //unknown error
 				console.log(`Error: ${parsed.error}`);
 				return;
 			}
 			
+			//approval code exists - authorization is complete.
+			//Get and store refresh tokens
 			const appCode = parsed.query.approvalCode;
 			if(appCode) {
 				this.token = await this.oAuth2Client.getToken(appCode);
