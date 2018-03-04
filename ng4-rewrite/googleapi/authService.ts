b2928ec3ecffb40cb757aa8a3b7223c713d33ff3
Aman Bhimani <amanb014@gmail.com>
2018-03-03 23:11:29 -0500

    improving and completing oauth process

--- ng4-rewrite/googleapi/authService.ts
+++ ng4-rewrite/googleapi/authService.ts
@@ -3,10 +3,10 @@ import { app, BrowserWindow } from 'electron';
 const http = require('http');
 const url = require('url');
 const querystring = require('querystring');
-const opn = require('opn');
 import { authKeys } from './auth';
 
 export default class AuthService {
+	static token:any;
 	static SCOPES:string[] = [
 		'https://www.googleapis.com/auth/youtube',
 		'https://www.googleapis.com/auth/youtube.readonly',
@@ -39,9 +39,37 @@ export default class AuthService {
 		});
 		this.authWin.loadURL(this.getAuthUrl(this.oAuth2Client));
 		this.authWin.on('closed', this.onClose);
+
+		this.authWin.webContents.on('will-navigate', (event, url) => {
+			this.handleNavigate(url);
+		});
+
+		// this.authWin.webContents.on('did-get-redirect-request', (event, oldUrl, newUrl) => {
+		// 	this.handleNavigate(newUrl);
+		// })
 	}
 
 	static onClose() {
 		this.authWin = null;
+		console.log('Authorization was cancalled by user.');
 	}
+
+	static async handleNavigate(newUrl:string) {
+		const parsed = url.parse(newUrl, true);
+
+		if(parsed) {
+			if(parsed.error) {
+				console.log(`Error: ${parsed.error}`);
+				return;
+			}
+			
+			const appCode = parsed.query.approvalCode;
+			if(appCode) {
+				this.token = await this.oAuth2Client.getToken(appCode);
+				this.authWin.removeAllListeners('closed');
+				this.authWin.close();
+			}
+		}
+	}
+
 }
