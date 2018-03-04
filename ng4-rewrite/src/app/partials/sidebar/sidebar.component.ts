2a095815c6eea4494ad3762ab345bd801f6e60e0
Aman Bhimani <amanb014@gmail.com>
2018-03-04 16:35:43 -0500

    sidebar contains list of playlist-items (without playlist-list component

--- ng4-rewrite/src/app/partials/sidebar/sidebar.component.ts
+++ ng4-rewrite/src/app/partials/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OAuthService } from './../../providers/oauth.service';
 import { ElectronService } from '../../providers/electron.service';
+import { Playlist } from '../../models/Playlist';
 
 @Component({
 	selector: 'app-sidebar',
@@ -9,11 +10,20 @@ import { ElectronService } from '../../providers/electron.service';
 })
 export class SidebarComponent implements OnInit {
 	sidebarHeader: string = 'Sign In With Google';
+	
+	playlist:Playlist = new Playlist({
+		title: 'Some playlist on YouTubewith Long title',
+		channelName: 'Aman Bhimani',
+		description: 'This is some description of the playlist listed here.'
+	});
 
 	constructor(private electronService: ElectronService) {
 
 	}
-	ngOnInit() { }
+	ngOnInit() {
+
+
+	}
 
 	loginHandler() {
 		this.electronService.ipcRenderer.send('authorize');
