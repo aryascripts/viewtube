4a0924babba9bbc0dc675d525f434723c9517cb4
Aman Bhimani <amanb014@gmail.com>
2018-03-06 18:26:56 -0500

    using observables in resizable directive

--- ng4-rewrite/src/app/directives/resizable.directive.ts
+++ ng4-rewrite/src/app/directives/resizable.directive.ts
@@ -1,13 +1,17 @@
 import { Directive, ElementRef, HostListener, 
 	Input, Output, OnInit, EventEmitter} from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import { Subscription } from 'rxjs/Subscription';
 
 @Directive({
 	selector: '[app-resize]'
 })
 export class ResizeDirective implements OnInit {
-	dragging:boolean;
 	startWidth:number;
 	startHeight:number;
+
+	mousemoveEvent: Subscription;
 
 	@Input('resizeMe') elementToResize:HTMLElement;
 	@Input() maxSize:number;
@@ -17,11 +21,9 @@ export class ResizeDirective implements OnInit {
 
 	@Output() mouseMoved = new EventEmitter();
 	
-
 	constructor(private el:ElementRef) { }
 
 	ngOnInit() {
-		this.dragging = false;
 		this.el.nativeElement.style.cursor = 'col-resize';
 	}
 
@@ -30,23 +32,26 @@ export class ResizeDirective implements OnInit {
 		event.preventDefault();
 		this.startWidth = this.elementToResize.clientWidth;
 		this.startHeight = this.elementToResize.clientHeight;
-		this.dragging = true;
+
+		this.mousemoveEvent = Observable.fromEvent(document, 'mousemove')
+															.subscribe(this.onMouseMove.bind(this));
 	}
 
 	@HostListener('document:mouseup') 
 	onMouseUp() {
-		this.dragging = false;
+		if(this.mousemoveEvent)
+			this.mousemoveEvent.unsubscribe();
 	}
 
-	@HostListener('document:mousemove', ['$event'])
 	onMouseMove(event: MouseEvent) {
-		if(this.dragging) {
 			let newNum:number = null, diff:number = 0;
 			if(this.vertical) {
 				diff = event.clientX - this.startWidth;
 				newNum = this.startWidth + diff;
 				if(newNum > this.minSize && newNum < this.maxSize)
 					this.elementToResize.style.width = newNum + 'px';
+				
+				console.log(newNum);
 			}
 			else {
 				diff = event.clientY - this.startHeight;
@@ -54,6 +59,6 @@ export class ResizeDirective implements OnInit {
 				if(newNum > this.minSize && newNum < this.maxSize)
 					this.elementToResize.style.height = newNum + 'px';
 			}
-		}
+		// }
 	}
 }
