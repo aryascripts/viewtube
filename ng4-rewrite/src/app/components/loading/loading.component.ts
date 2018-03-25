8f34cbf62ce703d56158ba7926acda908f765f0b
Aman Bhimani <amanb014@gmail.com>
2018-03-25 12:45:39 -0400

    loading component stub

---
+++ ng4-rewrite/src/app/components/loading/loading.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Playlist } from './../../models/Playlist';
+
+@Component({
+	selector: 'app-loading',
+	template: ``,
+	styleUrls: ['loading.component.scss']
+})
+export class LoadingComponent {
+	
+}
