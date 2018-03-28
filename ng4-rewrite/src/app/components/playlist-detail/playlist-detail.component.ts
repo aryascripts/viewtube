ac770f25bedb6392c04e9ef737ce93d6502448dc
Aman Bhimani <amanb014@gmail.com>
2018-03-27 20:13:26 -0400

    playlist-detail component stub

---
+++ ng4-rewrite/src/app/components/playlist-detail/playlist-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-playlist-detail',
+	templateUrl: 'playlist-detail.component.html',
+	styleUrls: ['playlist-detail.component.scss']
+})
+export class PlaylistDetailComponent {
+
+	constructor() {
+		
+	}
+}
