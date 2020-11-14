74eaea07807ac6478719376315dbf0fd9f99c471
Aman Bhimani <amanb014@gmail.com>
2020-11-14 12:49:20 -0500

    Updated home page and styling (no sidebar)

---
+++ src/app/partials/settings/settings.component.ts
@@ -0,0 +1,16 @@
+import { OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-settings',
+	templateUrl: 'settings.component.html',
+	styleUrls: ['settings.component.scss']
+})
+export class SettingsComponent implements OnInit {
+
+    ngOnInit(): void {
+        
+    }
+
+
+}
